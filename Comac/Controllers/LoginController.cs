e9818efe6a3d1f9b5dccbe95ba2abb1c46906cce
MehmetTatgil <46137971+MehmetTatgil@users.noreply.github.com>
2019-01-06 03:34:32 +0300

    login sayfası deneme

--- Comac/Controllers/LoginController.cs
+++ Comac/Controllers/LoginController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Comac.Models;
 namespace Comac.Controllers
 {
     public class LoginController : Controller
     {
+      
+
         // GET: Login
-        public ActionResult Index()
+        public ActionResult Login()
         {
-            int a = 5;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string name, string password)
+        {
+            if("admin".Equals(name)&&"123".Equals(password))
+            {
+                Session["user"] = new User() { Login = name, Name = "Kullanıcı" };
+
+                return RedirectToAction("Index", "Home");
+            }
             return View();
         }
     }
